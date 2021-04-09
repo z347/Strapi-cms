8bc7ef0f65064cc825d206ce26841e89e38e460a
z347 <shulo.vova@gmail.com>
2021-04-09 09:47:15 +0300

    install with mongoDB
    install without --quickstart

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '03059409398af85ecad93117cdbba1fb'),
+    },
+  },
+});
